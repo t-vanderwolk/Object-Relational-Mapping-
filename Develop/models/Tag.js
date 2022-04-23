d8ff52eda5c1358f9b358d6610a5629811adfa23
t-vanderwolk <97120854+t-vanderwolk@users.noreply.github.com>
2022-04-23 09:55:14 -0700

    resolved all isuues in models

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -3,10 +3,18 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
-
+//define collumns
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
